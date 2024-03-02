143d53ac0d8b2de826f23ac390905c4753c62437
Rama Mukti <155377652+ramamukti@users.noreply.github.com>
2024-03-02 23:57:00 +0700

    Update seeder.js

--- seeders/seeder.js
+++ seeders/seeder.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const db = require('../config/db');
 
-const sql = fs.readFileSync("./seeders/seed.sql", "utf-8");
+const sql = fs.readFileSync("../seeders/seed.sql", "utf-8");
 
 db.query(sql, (err) => {
     if(err){
